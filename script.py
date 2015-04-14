8c94973b51968ae3dd6608730cdf671e3ec8310b
trakons <isztl.david@gmail.com>
2015-04-14 16:05:49 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,72 +1,84 @@
 #!/usr/bin/env python
 
-stringa = "nid0[0254,4448,1602,4864,4866,5207,1010,3078,0685,0545,5189,0326,3559,5337,0018,0322,0426,0486,4286,4542,0966,1910,3582,0007,0845,0682,2474,1486,1478,1507,2239,0255,3477,0343],nid0004,nid00[100-103],nid050[01-04],nid040[06,07,09-11]"
-
-##########	VARIABILI
-nlist = 0
-finito = 0
-char = ""
-output = []
-##########
-
-stringa = stringa.translate(None, "nid")
-array = list(stringa)
-
-################
-def stampa(array):
-	tras = 0
-	if "[" in array:
-		tras = 1
-		array = array.split("[")
-		n1 = array[0]
-		n2 = array[1]
-		n2 = n2.split("]")
-		n2 = filter(lambda a: a != "," and a != "", n2)
-		for jn in n2:
-			if "-" in jn:
-				arrayj = jn.split(",")
-				for jj in arrayj:
-					#print jj + str("--")
-					if "-" in jj:
-						conta = jj.split("-")
-						for n in range(int(conta[0]),int(conta[1])+1):
-							if n < 10: output.append(str(n1) + str(0) + str(n))
-							if n > 9: output.append(str(n1) + str(n))
-					else:
-						output.append(output.append(str(n1) + str(jj)))
-			else:
-				arrayj = jn.split(",")
-				for cj in arrayj:
-					ris =  str(n1) + str(cj)
-					if len(ris) > 4: ris = ris[1:]
-					output.append(ris)
-		#return None
-	if tras == 0 and array.translate(None,",") != "": output.append(array.translate(None,","))
-	#return "0"
+stringa = "nid0[0254,4448,1602,4864,4866,5207,1010,3078,0685,0545,5189,0326,3559,5337,0018,0322,0426,0486,4286,4542,0966,1910,3582,0007,0845,0682,2474,1486,1478,1507,2239,0255,3477,0343],nid0004,nid00[100-103],nid050[01-04],nid040[06,07,09-11],nid[00002-00005]"
 
 
-################
+stringa = list(stringa)
 
-for c in array:
-	char = str(char) + str(c)
-	if c == ",":
-		finito = 1
-	elif c == "[":
-		nlist = 1
-	elif c == "]":
-		nlist = 0
 
-	if finito == 1 and nlist == 0:
-		#print "------------------"
-		if stampa(char) != "" or stampa(char) != "0": output.append(stampa(char))
-		finito = 0
-		char = ""
+lung = 4  #lunghezza della stampa
 
-for i in output:
-	if i != None:
-		if len(i) > 4:
-			print i[1:]
+
+parte = ""
+ris = []
+
+def range1(s):
+	del s[-1]
+	s = ''.join(map(str,s))
+	s = s.split('[')
+	p1 = s[0]
+	p2 = s[1].split(',')
+	p2fin = []
+	for j in p2:
+		if "-" in j:
+			n = j.split('-')
+			for jj in range (int(n[0]),int(n[1])+1):
+				if jj < 10:
+					p2fin.append(str(0)+str(jj))
+				else:
+					p2fin.append(jj)
 		else:
-			print i
+			p2fin.append(j)
+			
+	#print p1, p2fin
+	for xy in p2fin:
+		ris.append(str(p1) + str(xy))
+	
+def range2(s):
+	del s[-1]
+	s = ''.join(map(str,s))
+	s = s.split('[')
+	p1 = str(s[0])
+	p2 = s[1].split(',')
+	#print p1, p2[1]
+	for xy in p2:
+		ris.append(str(p1) + str(xy))
+	
+def elabora(t):
+	t = t.translate(None, "id")
+	t = list(t)
+	if t[0] == "[": t = ['0'] + t
+	if t[len(t)-1] == ",": del t[-1]
+	if "-" in t:
+		range1(t)
+	elif "," in t:
+		range2(t)
+	else:
+		ris.append(''.join(map(str,t)))
+
+
+for x in range (1,len(stringa)):
+	if stringa[x] == "n" and parte != "":
+		elabora(parte)
+		parte = ""
+	elif x == len(stringa)-1:
+		parte = str(parte) + str(stringa[x])
+		elabora(parte)
+	elif stringa != "n":
+		parte = str(parte) + str(stringa[x])
+	else:
+		print "Errore sconosciuto"
+		exit()
+
+def allunga(n,t):
+	stampa = ""
+	for j in range (n,lung):
+		stampa = str('0') + str(stampa)
+	print str(stampa) + str(t)
+
+for k in ris:
+	if len(k) > lung: print k[len(k)-lung:]
+	if len(k) < lung: allunga(len(k),str(k))
+	if len(k) == lung: print str(k)
 
 exit()
